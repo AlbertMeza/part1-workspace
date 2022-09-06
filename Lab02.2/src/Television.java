8ecd67d11e99525b7541ca4f06c5e80679d67f80
AlbertMeza <almez5202@gmail.com>
2022-09-06 15:04:07 -0700

    Completed Lab3.4

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,6 +1,25 @@
 public class Television {
+
+  public static final int MIN_VALUE = 0;
+  public static final int MAX_VALUE = 100;
+
+  private static int instanceCount = 0;
   private String brand;
-  private int volume;
+  private static int volume;
+
+  public Television(){
+    instanceCount++;
+  }
+
+  public Television(String brand){
+    this();
+    setBrand(brand);
+  }
+
+  public Television(String brand, int volume){
+    this(brand);
+    setVolume(volume);
+  }
 
   public void turnOn(){
     boolean isConnected = verifyInternetConnection();
@@ -9,6 +28,10 @@ public class Television {
 
   public void turnOff(){
     System.out.println("That's enough television on our " + this.brand + " for today");
+  }
+
+  public static int getInstanceCount() {
+    return instanceCount;
   }
 
   public String getBrand() {
@@ -24,7 +47,8 @@ public class Television {
   }
 
   public void setVolume(int volume) {
-    this.volume = volume;
+    if (volume < MAX_VALUE && volume > MIN_VALUE) this.volume = volume;
+    else System.out.printf("%d is invalid; volume must be between %d and %d (inclusive).%n", volume, MIN_VALUE, MAX_VALUE);
   }
 
   private boolean verifyInternetConnection(){
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,18 +3,23 @@ public class TelevisionClient {
   public static void main(String[] args) {
     Television tv1 = new Television();
     tv1.setBrand("Samsung");
-    tv1.setVolume(32);
+    tv1.setVolume(125);
 
-    Television tv2 = new Television();
-    tv2.setBrand("LG");
-    tv2.setVolume(12);
+    System.out.printf("%d television instances created%n", Television.getInstanceCount());
+
+    Television tv2 = new Television("LG", 12);
+
+    Television tv3 = new Television("Sony");
+
+    System.out.printf("%d television instances created%n", Television.getInstanceCount());
 
     tv1.turnOn();
     tv1.turnOff();
     tv2.turnOn();
     tv2.turnOff();
 
-    System.out.println(tv1.toString());
-    System.out.println(tv2.toString());
+    System.out.println(tv1);
+    System.out.println(tv2);
+    System.out.println(tv3);
   }
 }
